88993b749864f1a2b4a169bd97bf035fbdd7d411
Ubuntu <ubuntu@ip-172-31-9-182.ap-northeast-1.compute.internal>
2024-05-20 13:07:25 +0000

    fixed errors for docker deployment

--- backend/init-mongo.sh
+++ backend/init-mongo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 
-mongoimport --host mongo --db wanderlust --collection posts --file /docker-entrypoint-initdb.d/sample_posts.json --jsonArray
+mongoimport --db wanderlust --collection posts --file ./data/sample_posts.json --jsonArray
+#mongoimport --host mongo --db wanderlust --collection posts --file /docker-entrypoint-initdb.d/sample_posts.json --jsonArray
